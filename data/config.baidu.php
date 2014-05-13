6f1b5c9ca16d16b7953b780de817150ad335f19e
John <jhcao23@gmail.com>
2014-05-12 19:29:31 -0600

    add config.baidu.php

---
+++ data/config.baidu.php
@@ -0,0 +1,35 @@
+<?php
+// database host
+$db_host   = "sqld.duapp.com:4050";
+
+// database name
+$db_name   = "AmnGIFDNLxzTEDcwDjMK";
+
+// database username
+$db_user   = "4Ynq7ZTA2uGuxarCMGYYj6MU";
+
+// database password
+$db_pass   = "ERjHHBYPnGNxG51w6xLPYWhsgHsYgrSb";
+
+// table prefix
+$prefix    = "ecs_";
+
+$timezone    = "UTC";
+
+$cookie_path    = "/";
+
+$cookie_domain    = "";
+
+$session = "1440";
+
+define('EC_CHARSET','utf-8');
+
+define('ADMIN_PATH','admin');
+
+define('AUTH_KEY', 'this is a key');
+
+define('OLD_AUTH_KEY', '');
+
+define('API_TIME', '2014-05-13 00:29:40');
+
+?>
--- temp/query_caches/sqlcache_config_file_a86280dc9235ababae620e12c1911fa2.php
+++ temp/query_caches/sqlcache_config_file_a86280dc9235ababae620e12c1911fa2.php
@@ -1,5 +1,5 @@
 <?php
-$this->mysql_config_cache_file_time = 1399943080;
+$this->mysql_config_cache_file_time = 1399943420;
 $this->timeline = 0;
 $this->timezone = -21600;
 $this->platform = 'OTHER';
